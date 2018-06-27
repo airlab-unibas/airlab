74369c0d69d5f40dbe7a24a204c0fa88c8d0411b
Robin Sandkuehler <robin.sandkuehler@gmail.com>
2018-06-27 23:47:22 +0200

    change requirements

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 __version__ = '0.1'
 url = 'https://github.com/airlab-unibas/airlab'
 
-install_requires = ['SimpleITK, torch, numpy, matplotlib']
+install_requires = ['SimpleITK', 'torch, numpy', 'matplotlib']
 
 
 setup(
@@ -15,5 +15,5 @@ setup(
     url=url,
     keywords=['image registration'],
     install_requires=install_requires,
-    packages=find_packages(exclude=['build']),
+    packages=find_packages(exclude=['example']),
     ext_package='')
