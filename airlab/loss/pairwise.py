734886fc7118185606ec2ebc8ddb9cf73fd421c5
Robin Sandkühler <robin.sandkuehler@gmail.com>
2019-01-28 14:15:25 +0100

    add normalized gradient fields image loss

--- airlab/loss/pairwise.py
+++ airlab/loss/pairwise.py
@@ -380,3 +380,97 @@ class MI(_PairwiseImageLoss):
         ent_joint = -(p_joint * th.log2(p_joint + 1e-10)).sum()
 
         return -(ent_fixed_image + ent_moving_image - ent_joint)
+
+class NGF(_PairwiseImageLoss):
+    r""" Implementation of the Normalized Gradient Fields image loss.
+
+		Args:
+			fixed_image (Image): Fixed image for the registration
+			moving_image (Image): Moving image for the registration
+			fixed_mask (Tensor): Mask for the fixed image
+			moving_mask (Tensor): Mask for the moving image
+			epsilon (float): Regulariser for the gradient amplitude
+			size_average (bool): Average loss function
+			reduce (bool): Reduce loss function to a single value
+
+	"""
+
+    def __init__(self, fixed_image, moving_image, fixed_mask=None, moving_mask=None, epsilon=1e-5,
+                 size_average=True,
+                 reduce=True):
+        super(NGF, self).__init__(fixed_image, moving_image, fixed_mask, moving_mask, size_average, reduce)
+
+        self._name = "ngf"
+
+        self._dim = fixed_image.ndim
+        self._epsilon = epsilon
+
+        if self._dim == 2:
+            dx = (fixed_image.image[..., 1:, 1:] - fixed_image.image[..., :-1, 1:]) * fixed_image.spacing[0]
+            dy = (fixed_image.image[..., 1:, 1:] - fixed_image.image[..., 1:, :-1]) * fixed_image.spacing[1]
+
+            if self._epsilon is None:
+                with th.no_grad():
+                    self._epsilon = th.mean(th.abs(dx) + th.abs(dy))
+
+            norm = th.sqrt(dx.pow(2) + dy.pow(2) + self._epsilon ** 2)
+
+            self._ng_fixed_image = F.pad(th.cat((dx, dy), dim=1) / norm, (0, 1, 0, 1))
+
+            self._ngf_loss = self._ngf_loss_2d
+        else:
+            dx = (fixed_image.image[..., 1:, 1:, 1:] - fixed_image.image[..., :-1, 1:, 1:]) * fixed_image.spacing[0]
+            dy = (fixed_image.image[..., 1:, 1:, 1:] - fixed_image.image[..., 1:, :-1, 1:]) * fixed_image.spacing[1]
+            dz = (fixed_image.image[..., 1:, 1:, 1:] - fixed_image.image[..., 1:, 1:, :-1]) * fixed_image.spacing[2]
+
+            if self._epsilon is None:
+                with th.no_grad():
+                    self._epsilon = th.mean(th.abs(dx) + th.abs(dy) + th.abs(dz))
+
+            norm = th.sqrt(dx.pow(2) + dy.pow(2) + dz.pow(2) + self._epsilon ** 2)
+
+            self._ng_fixed_image = F.pad(th.cat((dx, dy, dz), dim=1) / norm, (0, 1, 0, 1, 0, 1))
+
+            self._ngf_loss = self._ngf_loss_3d
+
+    def _ngf_loss_2d(self, warped_image):
+
+        dx = (warped_image[..., 1:, 1:] - warped_image[..., :-1, 1:]) * self._moving_image.spacing[0]
+        dy = (warped_image[..., 1:, 1:] - warped_image[..., 1:, :-1]) * self._moving_image.spacing[1]
+
+        norm = th.sqrt(dx.pow(2) + dy.pow(2) + self._epsilon ** 2)
+
+        return F.pad(th.cat((dx, dy), dim=1) / norm, (0, 1, 0, 1))
+
+    def _ngf_loss_3d(self, warped_image):
+
+        dx = (warped_image[..., 1:, 1:, 1:] - warped_image[..., :-1, 1:, 1:]) * self._moving_image.spacing[0]
+        dy = (warped_image[..., 1:, 1:, 1:] - warped_image[..., 1:, :-1, 1:]) * self._moving_image.spacing[1]
+        dz = (warped_image[..., 1:, 1:, 1:] - warped_image[..., 1:, 1:, :-1]) * self._moving_image.spacing[2]
+
+        norm = th.sqrt(dx.pow(2) + dy.pow(2) + dz.pow(2) + self._epsilon ** 2)
+
+        return F.pad(th.cat((dx, dy, dz), dim=1) / norm, (0, 1, 0, 1, 0, 1))
+
+    def forward(self, displacement):
+
+        # compute displacement field
+        displacement = self._grid + displacement
+
+        # compute current mask
+        mask = super(NGF, self).GetCurrentMask(displacement)
+
+        self._warped_moving_image = F.grid_sample(self._moving_image.image, displacement)
+
+        # compute the gradient of the warped image
+        ng_warped_image = self._ngf_loss(self._warped_moving_image)
+
+        value = 0
+        for dim in range(self._dim):
+            value = value + ng_warped_image[:, dim, ...] * self._ng_fixed_image[:, dim, ...]
+
+        value = 0.5 * th.masked_select(-value.pow(2), mask)
+
+        return self.return_loss(value)
+
+
